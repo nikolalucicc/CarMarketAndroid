cf89b24405ac5fbb175b45863136fc89f2305461
NIkola Lucic <lucic2612@gmail.com>
2024-03-04 14:26:22 +0100

    Models and mappers edit

--- app/src/main/java/com/carmarket/model/dto/FavoriteDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.carmarket.model.dto
-
-data class FavoriteDTO (
-    val id: Long,
-    val user: UserDTO,
-    val ad: AdDTO
-)
--- app/src/main/java/com/carmarket/model/dto/ImageDTO.kt
+++
@@ -1,6 +0,0 @@
-package com.carmarket.model.dto
-
-data class ImageDTO(
-    val id: Long,
-    val url: String
-)
